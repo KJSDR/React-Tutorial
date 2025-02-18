76d433b0c445036c438e2be182935d04bd99138e
Killian <killian.delamarre-ruer@students.dominican.edu>
2025-02-18 04:52:44 -0800

    added react router

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,23 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './POPOSSpace.css';
 
+function POPOSSpace({ id, name, image, address, hours }) {
+  return (
+    <div className="POPOSSpace">
+      <Link to={`/details/${id}`}>
+        <img src={`${process.env.PUBLIC_URL}/images/${image}`} width="300" height="300" alt={name} />
+      </Link>
+      <h1>
+        <Link to={`/details/${id}`}>
+          {name}
+        </Link>
+      </h1>
+      <div>{address}</div>
+      <div className="POPOSHours">Hours: {hours}</div>
+    </div>
+  );
+}
 
-function POPOSSpace(props) {
-    const { name, image, address, hours } = props;
-    return (
-      <div className="POPOSSpace">
-        <img
-          src={`${process.env.PUBLIC_URL}/images/${image}`}
-          width="300"
-          height="300"
-          alt={name}
-        />
-        <h1>{name}</h1>
-        <div>{address}</div>
-        <div className="POPOSHours">Hours: {hours}</div>
-      </div>
-    );
-  }
-  
-  export default POPOSSpace;
-  
+export default POPOSSpace;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import POPOSList from './POPOSList';
+import About from './About';
+import POPOSDetails from './POPOSDetails';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<POPOSList />} />
+          <Route path="about" element={<About />} />
+          <Route path="details/:id" element={<POPOSDetails />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
