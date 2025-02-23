9cb0fe5a200cf719a6ccd2e6b605238846917c51
Killian <killian.delamarre-ruer@students.dominican.edu>
2025-02-22 22:42:15 -0800

    searching spaces

--- src/components/POPOSDetails/POPOSDetails.js
+++ src/components/POPOSDetails/POPOSDetails.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import data from '../../sfpopos-data.json';
+import data from '../../sfpopos-data.js';
 import './POPOSDetails.css';
+
 
 function POPOSDetails() {
   const { id } = useParams();
--- src/components/POPOSList/POPOSList.js
+++ src/components/POPOSList/POPOSList.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import POPOSSpace from '../POPOSSpace/POPOSSpace';
 import './POPOSList.css';
-import data from '../sfpopos-data.json';
-
+import data from '../sfpopos-data.js'; // Changed from JSON to JS file
 
 function POPOSList() {
-  const spaces = data.map(({ title, address, images, hours }, i) => {
-    return (
+  const [query, setQuery] = useState('');
+
+  const spaces = data
+    .filter(({ title, address }) => {
+      const inTitle = title.toLowerCase().includes(query.toLowerCase());
+      const inAddress = address.toLowerCase().includes(query.toLowerCase());
+      return inTitle || inAddress;
+    })
+    .map(({ id, title, address, images, hours }) => (
       <POPOSSpace
-        id={i}
-        key={title}
+        id={id}
+        key={`${title}-${id}`}
         name={title}
         address={address}
         image={images[0]}
         hours={hours}
       />
-    );
-  });
+    ));
 
-  return <div className="POPOSList">{spaces}</div>;
+  return (
+    <div className="POPOSList">
+      <form>
+        <input
+          value={query}
+          placeholder="Search"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {spaces}
+    </div>
+  );
 }
 
 export default POPOSList;
