0ad975bcfefc91afc34cfeaa15cffb7ca288d687
Killian <killian.delamarre-ruer@students.dominican.edu>
2025-02-23 01:13:38 -0800

    added random space

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,18 +1,18 @@
-// src/Title.js
-import './Title.css';
 import { NavLink } from 'react-router-dom';
+import RandomSpace from '../RandomSpace/RandomSpace';
+import './Title.css';
 
 function Title() {
   return (
     <div className="Title">
       <header>
         <h1>SFPOPOS</h1>
-        <div className="Title-Subtitle">
-          San Francisco Privately Owned Public Open Spaces
-        </div>
+        <div className="Title-Subtitle">San Francisco Privately Owned Public Open Spaces</div>
+
         <div>
           <NavLink className={({ isActive }) => isActive ? "nav-link-active" : "nav-link"} to="/">List</NavLink>
           <NavLink className={({ isActive }) => isActive ? "nav-link-active" : "nav-link"} to="/about">About</NavLink>
+          <RandomSpace /> {/* Added RandomSpace button */}
         </div>
       </header>
     </div>
